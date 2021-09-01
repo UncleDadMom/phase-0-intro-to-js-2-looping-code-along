6840f9ef307e57582004f2ff4a7ddc064aed4513
Patrick Wu <Ptrckwu@yahoo.com>
2021-09-01 17:05:33 -0400

    fixed countdown fx

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+    
+}
+*/
+let thankYous = []
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        thankYous.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return thankYous;
+}
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--
+    }
+}
